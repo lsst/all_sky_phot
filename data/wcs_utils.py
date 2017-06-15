ce6a71144967f7b2795fc8f8daf8189f38f8033d
Peter Yoachim <yoachim@uw.edu>
2017-06-15 16:36:34 -0700

    getting closer

--- data/wcs_utils.py
+++ data/wcs_utils.py
@@ -1,14 +1,7 @@
 import numpy as np
-from astropy.io import fits
-from astropy import wcs
-from astropy.coordinates import SkyCoord, EarthLocation, AltAz, ICRS, Longitude, Latitude
+from astropy.coordinates import SkyCoord, EarthLocation, AltAz, ICRS
 import astropy.units as u
 from astropy.time import Time
-from astropy.table import Table, vstack
-import matplotlib.pylab as plt
-from read_ybc import readYBC
-import lsst.all_sky_phot.wcs as asp
-from scipy.optimize import minimize
 
 # Some handy utilities for bootstrapping WCS solutions
 
@@ -27,7 +20,7 @@ def radec2altaz(ra, dec, mjd, location=lsst_location):
     return ack.alt.value, ack.az.value
 
 
-def apply_wcs_to_photometry(ptable, w, location=lsst_location):
+def apply_wcs_to_photometry(ptable, w, location=lsst_location, zp=None):
     """
     Take a photometry table and add ra and dec cols
 
@@ -52,27 +45,66 @@ def apply_wcs_to_photometry(ptable, w, location=lsst_location):
     ptable['az_wcs'] = coords.az
     ptable['ra_wcs'] = sky_coords.ra
     ptable['dec_wcs'] = sky_coords.dec
+
+    if zp is not None:
+        ptable['mag'] = -2.5*np.log10(ptable['residual_aperture_sum'].data) - zp
+
     return ptable
 
 
-def match_catalog(ptable, catalog, location=lsst_location):
+def match_catalog(ptable, catalog, cat_mags, location=lsst_location):
     """Match the photometry to a catalog
 
-    Add matched_alt, az, ra, dec columns. Maybe also matched star ID
+    Add matched_alt, az, ra, dec columns, and distance to match. Maybe also matched star ID
     """
 
-    phot_cat = SkyCoord(ra=ptable['ra_wcs'].value*u.degree, dec=ptable['dec_wcs'].value*u.degree)
+    good_coords = np.where((~np.isnan(ptable['ra_wcs'])) & (~np.isnan(ptable['dec_wcs'])))
+    phot_cat = SkyCoord(ra=ptable['ra_wcs'].value[good_coords]*u.degree,
+                        dec=ptable['dec_wcs'].value[good_coords]*u.degree)
     idx, d2d, d3d = phot_cat.match_to_catalog_sky(catalog)
 
-    ptable['ra_matched'] = catalog.ra[idx]
-    ptable['dec_matched'] = catalog.dec[idx]
+    # Clear any old columns
+    ptable['ra_matched'] = -666.
+    ptable['dec_matched'] = -666.
+    ptable['alt_matched'] = -666.
+    ptable['az_matched'] = -666.
+    ptable['d2d'] = -666.
+    ptable['matched_Vmag'] = -666.
+    ptable['bright_star_idx'] = -666
+    ptable['d2d'][good_coords] = d2d
+    ptable['matched_Vmag'][good_coords] = cat_mags[idx]
+    ptable['bright_star_idx'][good_coords] = idx
 
-    catalog.transform_to(AltAz, obstime=phot_cat['mjd'].max(), location=location)
-    ptable['alt_matched'] = catalog.alt[idx]
-    ptable['az_matched'] = catalog.az[idx]
-    ptable['d2d'] = d2d
+    ptable['ra_matched'][good_coords] = catalog.ra[idx]
+    ptable['dec_matched'][good_coords] = catalog.dec[idx]
+
+    time = Time(ptable['mjd'].max(), format='mjd')
+    ack = catalog.transform_to(AltAz(obstime=time, location=location))
+    ptable['alt_matched'][good_coords] = ack.alt[idx]
+    ptable['az_matched'][good_coords] = ack.az[idx]
+    ptable
 
     return ptable
 
 
+def xy_altaz_mapping(photo_array, d2d_limit=2., mag_limit=2.):
+    """Take an array that has been matched, and try to generate a good 
+    map that can be used to fit a better WCS.
+    """
+
+    # Maybe for each star, make a running median and robust rms for x,y,mag as 
+    # a function of time. Then reject outliers. 
+    # Array to save if we think a point is a good match
+    good_match = np.zeros(np.size(photo_array), dtype=bool)
+    meet_criteria = np.where((photo_array['d2d'] < d2d_limit) &
+                             (np.abs(photo_array['mag'] - photo_array['matched_Vmag']) < mag_limit))
+    good_match[meet_criteria] = True
+
+    # The unique stars that I should loop though
+    ustars = np.unique(photo_array['bright_star_idx'][good_match])
+
+
+
+
+    pass
 
