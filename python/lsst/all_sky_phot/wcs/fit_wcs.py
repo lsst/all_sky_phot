55d4beb7bb02d503a85bbe9e0ef002d8d14bdf80
Peter Yoachim <yoachim@uw.edu>
2017-06-21 16:32:20 -0700

    trying new fitting technique

--- python/lsst/all_sky_phot/wcs/fit_wcs.py
+++ python/lsst/all_sky_phot/wcs/fit_wcs.py
@@ -1,9 +1,12 @@
 import numpy as np
 from astropy.wcs import Sip
 from astropy import wcs
+from astropy.coordinates import SkyCoord, EarthLocation, AltAz, ICRS
+import astropy.units as u
+from astropy.time import Time
 
 # Let's make a new attempt to fit things, this time using the WCS object.
-__all__ = ['wcs_azp', 'wcs_zea']
+__all__ = ['wcs_azp', 'wcs_zea', 'wcs_refine']
 
 
 
@@ -143,3 +146,77 @@ class wcs_zea(wcs_azp):
             a = x0[self.a_ind].reshape((self.a_order + 1, self.a_order + 1))
             b = x0[self.b_ind].reshape((self.b_order + 1, self.b_order + 1))
             self.w.sip = Sip(a, b, self.sip_zeros_a, self.sip_zeros_b, self.w.wcs.crpix)
+
+
+def mag2quasi_dist(mag):
+    """Assume all stars have absolute mag of 10.
+    """
+    mu = mag - 10.
+    dist = 10.**(mu/5.+1)
+    return dist
+
+
+class wcs_refine(wcs_azp):
+    """Take a catalog of alt,az positions of known stars and minimize the d3 distance
+    """
+    def __init__(self, x, y, xy_mag, xy_mjd, ra, dec, rd_mag, location=None,
+                 a_order=0, b_order=0, crpix1=0, crpix2=0, alt_limit=15.):
+
+        self.alt_limit = alt_limit
+        # Assume all stars are V=10 for distance purposes
+        if location is None:
+            self.location = EarthLocation(lat=-30.2444*u.degree,
+                                     lon=-70.7494*u.degree,
+                                     height=2650.0*u.meter)
+        else:
+            self.location = location
+
+        self.x = x
+        self.y = y
+        self.dist = mag2quasi_dist(xy_mag)*u.pc
+
+        aa_dist = mag2quasi_dist(rd_mag)
+        ref_catalog = SkyCoord(ra=ra*u.degree, dec=dec*u.degree, distance=aa_dist*u.pc)
+        self.time = Time(xy_mjd, format='mjd')
+        self.ref_catalog = ref_catalog.transform_to(AltAz(obstime=self.time,
+                                                          location=self.location))
+        self.ref_catalog = self.ref_catalog[np.where(self.ref_catalog.alt.value > alt_limit)]
+
+        # The wcs object we'll be using
+        self.w = wcs.WCS(naxis=2)
+        self.w.wcs.crpix = [crpix1, crpix2]
+        # Fix the reference pixel to zenith
+        self.w.wcs.crval = [0, 90]
+        self.w.wcs.ctype = ["RA---AZP-SIP", "DEC--AZP-SIP"]
+
+        # Make a sip object with all zero values
+        self.a_order = a_order
+        self.b_order = b_order
+        n_a = int((a_order + 1.)**2)
+        n_b = int((b_order + 1)**2)
+
+        self.a_ind = np.arange(n_a) + 10
+        self.b_ind = np.arange(n_b) + self.a_ind.max() + 1
+
+        self.sip_zeros_a = np.zeros((a_order+1, a_order + 1))
+        self.sip_zeros_b = np.zeros((b_order+1, b_order + 1))
+        self.w.sip = None
+
+    def find_distances(self, x0):
+        self.set_wcs(x0)
+        detected_azs, detected_alts = self.w.all_pix2world(self.x, self.y, 0)
+        observed_cat = SkyCoord(detected_azs*u.degree, detected_alts*u.degree,
+                                distance=self.dist, frame=AltAz(obstime=self.time, location=self.location))
+        try:
+            indx, d2, d3 = self.ref_catalog.match_to_catalog_3d(observed_cat)
+            result = (indx, d2.value, d3.value)
+        except:
+            result = ([], np.inf, np.inf)
+        return result
+
+    def __call__(self, x0):
+        indx, d2, d3 = self.find_distances(x0)
+        result = np.sum(d2)
+        return result
+
+
