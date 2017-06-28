719aec569ffc5ff12fb9cd4abfcb035e849d301e
Peter Yoachim <yoachim@uw.edu>
2017-06-28 16:38:39 -0700

    running a better distortion fitter

--- python/lsst/all_sky_phot/wcs/fisheye_map.py
+++ python/lsst/all_sky_phot/wcs/fisheye_map.py
@@ -186,37 +186,28 @@ def fit_xyshifts(x, y, alt, az, wcs, max_shift=40., min_points=3, windowsize=200
 
 
 class dist_mapper_minimizer(object):
-    def __init__(self, catalog_u, catalog_v, mjd, kdtree, indx, ngrid):
+    def __init__(self, catalog_u, catalog_v, mjd, kdtree):
         self.u = catalog_u
         self.v = catalog_v
         self.mjd = mjd
         self.kdtree = kdtree
-        self.ngrid = ngrid
-        self.indx = indx
-
-        self.bins = np.arange(self.indx.min(), self.indx.max()+2, 1) - 0.5
 
     def __call__(self, x0):
-        xshifts = x0[0:self.ngrid]
-        yshifts = x0[self.ngrid:]
+        xshifts = x0[0]
+        yshifts = x0[1]
 
-        final_x = self.u + xshifts[self.indx]
-        final_y = self.v + yshifts[self.indx]
+        final_x = self.u + xshifts
+        final_y = self.v + yshifts
 
         distances, indx = self.kdtree.query(np.array([final_x, final_y, self.mjd]).T)
 
-        hist_median, bin_edges, bn = binned_statistic(self.indx, distances,
-                                                      bins=self.bins, statistic='median')
-        count_median, be, bn, =  binned_statistic(self.indx, distances,
-                                                      bins=self.bins, statistic='count')
-        good = np.where(count_median > 0)
-        result = np.mean(hist_median[good])
+        result = np.median(distances)
         # print 'mean of medians', result
         return result
 
 
 def distortion_mapper(observed_x, observed_y, observed_mjd, catalog_alt, catalog_az, catalog_mjd, wcs,
-                      mjd_multiplier=1e4, urange=[800, 5000], vrange=[0, 3700], num_points=5):
+                      mjd_multiplier=1e4, u_center=2000, v_center=2000, window=100, pad=20):
     """Try to find a set of distorions that do a good job matching the catalog alt, az positions to
     observed chip positions
     """
@@ -224,27 +215,31 @@ def distortion_mapper(observed_x, observed_y, observed_mjd, catalog_alt, catalog
     # Generate the u,v positions of catalog objects
     catalog_u, catalog_v = wcs.all_world2pix(catalog_az, catalog_alt, 0)
 
+    # Crop down to the region of the chip we are interested in
+    good = np.where((catalog_u < u_center+window) & (catalog_u > u_center-window) &
+                    (catalog_v < v_center+window) & (catalog_v > v_center-window))
+    catalog_u = catalog_u[good]
+    catalog_v = catalog_v[good]
+    catalog_mjd = catalog_mjd[good]
+
+    good = np.where((observed_x < u_center+window+pad) & (observed_x > u_center-window-pad) &
+                    (observed_y < v_center+window+pad) & (observed_y > v_center-window-pad))
+    observed_x = observed_x[good]
+    observed_y = observed_y[good]
+    observed_mjd = observed_mjd[good]
+
+    # If we have nothing to match, return None
+    if (catalog_u.size == 0) | (observed_x.size == 0):
+        return None
+
     # Let's make things 3 dimensional, so we can use the kdtree to do the mjd sorting for us
     observed_kd = KDTree(np.array([observed_x, observed_y, observed_mjd*mjd_multiplier]).T)
 
-    # Make a grid of u,v reference points. Assign each catalog point to an offset point.
-    ugrid = np.linspace(np.min(urange), np.max(urange), num_points)
-    vgrid = np.linspace(np.min(vrange), np.max(vrange), num_points)
-
-    ugrid, vgrid = np.meshgrid(ugrid, vgrid)
-    ugrid = ugrid.ravel()
-    vgrid = vgrid.ravel()
-
-    ref_kd = KDTree(np.array([ugrid, vgrid]).T)
-
-    # Find the reference index for each catalog observation
-    distances, indx = ref_kd.query(np.array([catalog_u, catalog_v]).T)
-
     fun = dist_mapper_minimizer(catalog_u, catalog_v, catalog_mjd*mjd_multiplier,
-                                observed_kd, indx, ugrid.size)
-    x0 = np.zeros(ugrid.size*2, dtype=float)
-    result = minimize(fun, x0, method='Powell', options={'maxiter': 3})
+                                observed_kd)
+    x0 = np.array([0., 0.])
+    result = minimize(fun, x0, method='Powell')
 
-    return result, ugrid, vgrid
+    return result
 
 
